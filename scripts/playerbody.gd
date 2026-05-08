bf45c3db8c4d563c17b38d57821dfea489472a4f
Kai Toor <kaitoor9@gmail.com>
2026-05-08 11:30:19 -0700

    Added particles and ship tilting

--- scripts/playerbody.gd
+++ scripts/playerbody.gd
@@ -1,28 +1,33 @@
 extends Node3D
+
 @export var forwardSpeed: float = 10
-@export var speedIncrease: float = 1;
+@export var speedIncrease: float = 1
 @export var sensitivity: float = .5
-@export var moveLerp: float = 1;
-@export var _range: float = 50;
+@export var moveLerp: float = 1
+@export var _range: float = 50
 @export var tiltAmount: float = 2
 @export var tiltLerp: float = 5
+@export var levelReturn: float = 1.5
 
 signal died
 
 @onready var scoreText = $"../CanvasLayer/Label"
+
 var targetPos: Vector3
 var velocity: Vector3
-var isDead: bool = false;
+var isDead: bool = false
 
-# Called when the node enters the scene tree for the first time.
+var visual_basis: Basis = Basis.IDENTITY
+var pitch_rate: float = 0.0
+var roll_rate: float = 0.0
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		targetPos.x -= event.relative.x * sensitivity #x in inverted
-		targetPos.y -= event.relative.y * sensitivity #y is inverted
+		targetPos.x -= event.relative.x * sensitivity
+		targetPos.y -= event.relative.y * sensitivity
 		targetPos.x = clamp(targetPos.x, -_range/2, _range/2)
 		targetPos.y = clamp(targetPos.y, -_range/2, _range/2)
 	elif event is InputEventKey:
@@ -30,22 +35,30 @@ func _unhandled_input(event: InputEvent) -> void:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		elif event.keycode == KEY_R:
 			get_tree().reload_current_scene()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	var prev_position = position
-	if (isDead):
+	if isDead:
 		return
 	position.x = lerp(position.x, targetPos.x, moveLerp * delta)
 	position.y = lerp(position.y, targetPos.y, moveLerp * delta)
 	position.z += forwardSpeed * delta
-	forwardSpeed += speedIncrease * delta;
-	
+	forwardSpeed += speedIncrease * delta
+
 	velocity = position - prev_position
-	scoreText.text = str(roundi(position.z/10))
-	
-	rotation.y = lerp(rotation.y, -velocity.x * tiltAmount, tiltLerp * delta)
-	rotation.x = lerp(rotation.x, velocity.y * tiltAmount, tiltLerp * delta)
-	
+	scoreText.text = str(roundi(position.z / 10))
+
+	_update_visual_rotation(delta)
+
+func _update_visual_rotation(delta: float) -> void:
+	if velocity.length() < 0.001:
+		return
+	var target_basis = Basis.looking_at(velocity.normalized(), Vector3.UP)
+	var bank_angle: float = velocity.x * tiltAmount * 3.0
+	target_basis = target_basis.rotated(target_basis.z, bank_angle)
+	visual_basis = visual_basis.slerp(target_basis, tiltLerp * delta)
+	transform.basis = visual_basis.orthonormalized()
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("obstacle"):
 		die()
